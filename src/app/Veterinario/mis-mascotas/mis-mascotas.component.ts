c0ed02e7d983b6fa361fa29148ef1ca266e923d4
alejo190404 <alejo190404@gmail.com>
2024-04-05 15:25:50 -0500

    Mostrar mascotas por veterinario implementado. Problema commit anterior solucionado

--- src/app/Veterinario/mis-mascotas/mis-mascotas.component.ts
+++ src/app/Veterinario/mis-mascotas/mis-mascotas.component.ts
@@ -22,7 +22,8 @@ export class MisMascotasComponent {
 
     ngOnInit(): void {
       this.route.paramMap.subscribe(params => {
-        this.vetId1 = params.get('id') as string;
+        this.vetId1 = String(params.get('id'));
+        this.vetId1 = this.router.url.split('id=')[1].split('&')[0];
         console.log(this.vetId1)
         this.servicioMascota.getMascotasByVeterinarioId(Number(this.vetId1)).subscribe(mascotas => {
           this.mascotaList = mascotas;
