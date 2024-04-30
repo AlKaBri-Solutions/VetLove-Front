423cd90f0257b082639a7a04de6cf2ec5073f89c
J-Brinez <juanf.brinez@gmail.com>
2024-04-30 12:12:57 -0500

    Swiper funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CommonModule } from '@angular/common';
@@ -73,7 +73,8 @@ register();
     DevExtremeModule,
   ],
   providers: [DatePipe],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
   ngOnInit() {
